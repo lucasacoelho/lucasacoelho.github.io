c5bb90b380f47151a26e23056dee8c5c0bec90e0
Lucas Almeida Coelho <89714517+lucasacoelho@users.noreply.github.com>
2022-08-22 17:41:17 -0300

    Update conversao.js

--- conversao.js
+++ conversao.js
@@ -1,21 +1,17 @@
 function calculo() {
     var tipo = document.getElementById("tipo").value;
     var altura = document.getElementById("altura").value;
-    var cm = document.getElementById("cm").value;
-    var feets = document.getElementById("feets").value;
     var type = document.getElementById("type").value;
     var peso = document.getElementById("peso").value;
-    var kg = document.getElementById("kg");
-    var pounds = document.getElementById("pounds");
     let resultado_pes = document.getElementById("resultado_pes");
     let resultado_alt = document.getElementById("resultado_alt");
-    let resultado = document.getElementById("result");
     if (altura == '' && peso == '') {
         resultado_alt.innerHTML =("Preencha os valores!")
     }else{  
         if (tipo === "cm") {
             var resp = altura / 30.48
             resultado_alt.innerHTML = (`a sua altura é ${resp.toFixed(1)} ft`)
+            resultado_pes.innerHTML = ('')
             if (type === "kilograms") {
                 var resp = peso * 2.205
                 resultado_pes.innerHTML = (` e o seu peso é ${resp.toFixed(3)} lbs`)
@@ -35,4 +31,43 @@ function calculo() {
             }
         }
     }
+}
+function transformar(){
+    var tipo = document.getElementById("tipo_grau1").value;
+    var celsius = document.getElementById("celsius").value;
+    var faren = document.getElementById("faren").value;
+    var kelvin = 273.15;
+    var type = document.getElementById("tipo_grau2").value;
+    var valor = document.getElementById("valor").value;
+    var result = document.getElementById("resultado_temp")
+    if (valor == "") {
+        result.innerHTML =("Preencha os valores!")
+    }if (tipo === "°C" && type === "°C"){
+        result.innerHTML = (`${valor} °C`)
+    }if (tipo === "°F" && type === "°F"){
+        result.innerHTML = (`${valor} °F`)
+    }if (tipo === "K" && type ==="K"){
+        result.innerHTML = (`${valor} K`)
+    }else{  
+        if (tipo === "°C" && type === "°F") {
+           let resp = ((valor * 9)/5 + 32)
+           result.innerHTML = (`${valor} °C é igual a ${resp.toFixed(2)} °F`)
+        } if (tipo === "°C" && type === "K") {
+            let value = parseFloat(valor)
+            let resp = (value + 273.15)
+            result.innerHTML = (`${valor} °C é igual a ${resp.toFixed(3)} K`)
+        }if (tipo === "°F" && type === "°C") {
+            let resp = (((valor - 32)*5)/9)
+            result.innerHTML = (`${valor} °F é igual a ${resp.toFixed(0)} °C`)
+        }if (tipo === "°F" && type === "K") {
+            let resp = (((valor - 32)*5)/9 + 274.15)
+            result.innerHTML = (`${valor} °F é igual a ${resp.toFixed(3)} K`)
+        }if (tipo === "K" && type === "°C") {
+            let resp = (valor - 273.15)
+            result.innerHTML = (`${valor} K é igual a ${resp.toFixed(0)} °C`)
+        }if (tipo === "K" && type === "°F") {
+            let resp = (((valor - 273.15) * 9)/5 + 32)
+            result.innerHTML = (`${valor} K é igual a ${resp.toFixed(2)} °F`)
+        }
+    }   
 }
